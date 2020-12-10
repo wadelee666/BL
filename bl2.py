e82774062d0d309481ff93ec6dcb965a48800671
wadelee666 <49467762+wadelee666@users.noreply.github.com>
2020-12-10 11:13:20 +0800

    Update bl2.py

--- bl2.py
+++ bl2.py
@@ -1,5 +1,6 @@
 import bluetooth
 
+socket.setdefaulttimeout(5)
 esp = 'C4:4F:33:3A:5B:83'
 port = 1
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -7,17 +8,15 @@ sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
  
 while True:
 	a = input()
+	sock.connect((esp,port))
 	if a == 'o':
-		sock.connect((esp,port))
 		sock.send('g'.ljust(16))		# Windows和 Linux應該可以直接發送
 		data = sock.recv(1)
+		sock.close()
 		print(data)
-	if a == 'h':
-		sock.connect((esp,port))
+	if a == 'H':
 		sock.send('H')
-	if a == 'l':
-		sock.connect((esp,port))
+		sock.close()
+	if a == 'L':
 		sock.send('L')
-	if a == 'k':
-		break
-sock.close()
+		sock.close()
