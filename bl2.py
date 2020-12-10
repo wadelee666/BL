3a2984b6c4fd6bfd5e86d02250981068a74e3303
wadelee666 <49467762+wadelee666@users.noreply.github.com>
2020-12-10 11:06:25 +0800

    Update bl2.py

--- bl2.py
+++ bl2.py
@@ -11,15 +11,13 @@ while True:
 		sock.connect((esp,port))
 		sock.send('g'.ljust(16))		# Windows和 Linux應該可以直接發送
 		data = sock.recv(1)
-		sock.close()
 		print(data)
 	if a == 'h':
 		sock.connect((esp,port))
 		sock.send('H')
-		sock.close()
 	if a == 'l':
 		sock.connect((esp,port))
 		sock.send('L')
-		sock.close()
 	if a == 'k':
 		break
+sock.close()
