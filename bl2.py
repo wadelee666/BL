26aaebd8a54f8e3eac24a093fb7d6b15085ebecf
wadelee666 <49467762+wadelee666@users.noreply.github.com>
2020-12-10 10:57:18 +0800

    Update bl2.py

--- bl2.py
+++ bl2.py
@@ -13,11 +13,13 @@ while True:
 		data = sock.recv(1)
 		sock.close()
 		print(data)
-	if a == 'H':
+	if a == 'h':
 		sock.connect((esp,port))
 		sock.send('H')
 		sock.close()
-	if a == 'L':
+	if a == 'l':
 		sock.connect((esp,port))
 		sock.send('L')
 		sock.close()
+	if a == 'k':
+		break
